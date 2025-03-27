bc761092d70a472dbe816c43b35d98e24edaf60e
Marco Ramírez <marco.ramirez@rzerocorp.com>
2025-03-27 10:02:14 -0600

    Changed to REST API implementation

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-from my_resume import MyResume
-import json
+from src.app.cv_app import CVApp
 
 if __name__ == '__main__':
-    with open('config.json', 'r') as file:
-        config = json.load(file)
-
-    username = config['credentials']['username']
-    password = config['credentials']['password']
-    profile_url = config["profileUrl"]
-
-    resume = MyResume(username, password, profile_url)
-    resume.login()
-
-    print(json.dumps(resume.getResume(), indent=4))
+    app = CVApp()
+    app.run()
---
+++ src/app/cv_app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import json
+from src.scraper.my_resume import MyResume
+
+class CVApp:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.__registerRoutes()
+
+    def __registerRoutes(self):
+        @self.app.route('/api/generate-cv', methods=['POST'])
+        def generateCV():
+            data = request.get_json()
+
+            username = data['username']
+            password = data['password']
+            profile_url = data['profileUrl']
+
+            resume = MyResume(username, password, profile_url)
+            try:
+                resume.login()
+                return json.dumps(resume.getResume(), indent=4)
+            except Exception as e:
+                return jsonify({"error": str(e)}), 500          
+
+    def run(self):
+        self.app.run(debug=True)
